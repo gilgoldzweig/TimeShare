68b0e6ba1bf303e651865c7b50ce55b078895fad
gilgoldzweig <gil5841@gmail.com>
2017-03-30 23:01:55 +0300

    added important utils

--- app/src/main/java/cc/time_share/android/activities/MainActivity.java
+++ app/src/main/java/cc/time_share/android/activities/MainActivity.java
@@ -1,15 +1,21 @@
 package cc.time_share.android.activities;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import cc.time_share.android.R;
+import cc.time_share.android.databinding.ActivityMainBinding;
+import cc.time_share.android.models.User;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding mainBinder = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User();
+        user.setName("Anis Abboud");
+        mainBinder.setUser(user);
     }
 }
--- app/src/main/java/cc/time_share/android/server/ServerHandler.java
+++ app/src/main/java/cc/time_share/android/server/ServerHandler.java
@@ -1,9 +1,24 @@
 package cc.time_share.android.server;
 
 
+import cc.time_share.android.models.Request;
+import cc.time_share.android.models.User;
+
 /**
  * Created by gilgoldzweig on 30/03/2017.
  */
 
 public class ServerHandler {
+    private static final ServerHandler ourInstance = new ServerHandler();
+
+    public static ServerHandler getInstance() {
+        return ourInstance;
+    }
+
+    private ServerHandler() {
+    }
+
+    public User getUserFromServer() {
+        return new User();
+    }
 }
