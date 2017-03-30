6cc7404e641a4ebcc658250e500de7935549c556
gilgoldzweig <gil5841@gmail.com>
2017-03-30 21:28:33 +0300

    pre made needed files and dependencies

--- app/src/main/java/goldzweigapps/com/timeshare/models/Profile.java
+++
@@ -1,8 +0,0 @@
-package goldzweigapps.com.timeshare.models;
-
-/**
- * Created by gilgoldzweig on 30/03/2017.
- */
-
-public class Profile {
-}
--- app/src/main/java/goldzweigapps/com/timeshare/server/ServerHandler.java
+++
@@ -1,9 +0,0 @@
-package goldzweigapps.com.timeshare.server;
-
-
-/**
- * Created by gilgoldzweig on 30/03/2017.
- */
-
-public class ServerHandler {
-}
