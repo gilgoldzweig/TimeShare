fb0afd8f6bad8ee07fcad6f315591ca8c0273075
gilgoldzweig <gil5841@gmail.com>
2017-03-31 23:14:13 +0300

    added missing files

--- app/src/main/java/cc/time_share/android/activities/HomeActivity.java
+++ app/src/main/java/cc/time_share/android/activities/HomeActivity.java
@@ -6,16 +6,24 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import cc.time_share.android.R;
+import goldzweigapps.tabs.Builder.EasyTabsBuilder;
+import goldzweigapps.tabs.View.EasyTabs;
 
-public class Home extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
     @BindView(R.id.toolbar_main)
     Toolbar mToolBar;
-    @BindView(R.id.fab_create)
+    @BindView(R.id.easy_tabs)
+    EasyTabs mEasyTabs;
+    @BindView(R.id.fab_create_new)
     FloatingActionButton mCreateFab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+        EasyTabsBuilder easyTabsBuilder = EasyTabsBuilder.with(mEasyTabs);
     }
 }
