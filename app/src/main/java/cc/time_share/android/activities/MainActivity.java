2f200f94c90bc1442ab534fce7dd4d6e588cc9cc
Anis Abboud <anis.abboud@gmail.com>
2017-03-31 13:55:04 +0300

    Use ChildEventListener for Requests

--- app/src/main/java/cc/time_share/android/activities/MainActivity.java
+++ app/src/main/java/cc/time_share/android/activities/MainActivity.java
@@ -45,8 +45,7 @@ import cc.time_share.android.models.Request;
 import cc.time_share.android.server.ServerHandler;
 
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,
-        ValueEventListener, ChildEventListener {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     private static final String TAG = MainActivity.class.getSimpleName();
     private ServerHandler mServerHandler;
     @BindView(R.id.toolbar_main)
@@ -70,7 +69,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.mapFragmentView);
         mServerHandler = ServerHandler.getInstance();
-        mServerHandler.setRequestListener(this);
+
+        subscribeToRequests();
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             requestPermissions(permissions, PERMISSION_KEY);
@@ -92,46 +92,41 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
-    @Override
-    public void onDataChange(DataSnapshot dataSnapshots) {
+    private void subscribeToRequests() {
+        mServerHandler.setRequestsListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Request request = dataSnapshot.getValue(Request.class);
+                Log.d(TAG, "onDataChange: " + request.getTitle());
 
-        for (DataSnapshot dataSnapshot : dataSnapshots.getChildren()) {
-            Request request = dataSnapshot.getValue(Request.class);
-            Log.d(TAG, "onDataChange: " + request.getTitle());
+                mGoogleMap.addMarker(new MarkerOptions()
+                        .flat(false)
+                        .title(request.getTitle())
+                        .snippet(request.getDescription())
+                        .position(new LatLng(request.getLatitude(),
+                                request.getLongitude())));
+            }
 
-            mGoogleMap.addMarker(new MarkerOptions()
-                    .flat(false)
-                    .title(request.getTitle())
-                    .snippet(request.getDescription())
-                    .position(new LatLng(request.getLatitude(),
-                            request.getLongitude())));
-        }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
 
-    }
+            }
 
-    @Override
-    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
 
-    }
+            }
 
-    @Override
-    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
 
-    }
+            }
 
-    @Override
-    public void onChildRemoved(DataSnapshot dataSnapshot) {
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
-    }
-
-    @Override
-    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-    }
-
-    @Override
-    public void onCancelled(DatabaseError databaseError) {
-
+            }
+        });
     }
 
     @Override
@@ -158,6 +153,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
         return super.onOptionsItemSelected(item);
     }
+
     @OnClick(R.id.fab_create)
     public void createFabAction(View view) {
         Intent createIntent = new Intent(MainActivity.this, CreateActivity.class);
